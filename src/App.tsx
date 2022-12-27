b6432d3caae370b9def7fb373e2965052ddc8d0b
Kacper <kacper.tylec1999@gmail.com>
2022-12-27 19:36:10 +0100

    installing packages, initializing redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
